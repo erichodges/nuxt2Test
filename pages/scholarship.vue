4ac6eb84571876eda6cb9202e3cd7982fcb7e7ec
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2018-10-15 07:26:01 -0700

    added text changes on scholarship page

--- pages/scholarship.vue
+++ pages/scholarship.vue
@@ -3,14 +3,14 @@
     <h1>Discovery Scholarship</h1>
     <br>
     <p>
-      In 1984 the Marin branch of the Music Teachers’ Association of California (MTAC) founded the Discovery scholarship program, a California 501(c)3 non-profit corporation, for the purpose of providing private music lessons to children from low-income families. The public schools do what they can in music education with limited funds but they have never been able to offer individual lessons to their students. While upper and middle-income families can provide their children with private lessons, low-income families do not have the wherewithal to do so. Discovery Scholarship attempts to fill that gap for at least some families. Discovery Scholarship started with three students in 1984 and has grown to twelve students.
+      In 1984 the Marin branch of the Music Teachers’ Association of California (MTAC) founded the Discovery scholarship program, a California 501(c)3 non-profit corporation, for the purpose of providing private music lessons to children from low-income families. Discovery Scholarship attempts to fill that gap for at least some families. Discovery Scholarship started with three students in 1984 and has grown to twelve students.
     </p>
-      Lessons are offered in a wide variety of instruments.  Participating teachers agree to accept a significantly reduced annual teaching fee. Recipients are chosen through a rigorous screening process involving an extensive written application, documentation of financial need, letters of recommendations from teachers (and others) and a personal interview with the three-member Scholarship Committee.
+      Lessons are offered in a wide variety of instruments. Recipients are chosen through a rigorous screening process involving an extensive written application, documentation of financial need, letters of recommendations from teachers (and others) and a personal interview with the three-member Scholarship Committee.
     <p>
       The Committee carefully reviews all aspects of the application and makes its decisions based on a wide variety of factors, with financial need being the uppermost. As long as the financial need remains and the student progresses satisfactorily, the Scholarship is guaranteed to continue through 12th grade.
     </p>
     <p>
-      Discovery students come from a diverse variety of ethnic backgrounds. Graduates of the program have become inventors, entrepreneurs, teachers, musicians, film composers, business professionals and photographers, among others. Students who remain in the program through senior year generally go on to college, including some of the best-known schools in the United States (Duke, George Washington University, Northwestern, Harvard, Columbia, the Culinary Institute of America at Hyde Park, etc.).
+      Discovery students come from diverse backgrounds. Graduates of the program have become inventors, entrepreneurs, teachers, musicians, film composers, business professionals and photographers, among others. Students who remain in the program through senior year generally go on to college, including some of the best-known schools in the United States (Duke, George Washington University, Northwestern, Harvard, Columbia, the Culinary Institute of America at Hyde Park, etc.).
     </p>
     <p>
       Discovery is financed through an annual fund-raising mailing, grants from several local non-profits and by many local Marin county residents who believe that music can offer a window into a world not generally available to low-income children.
@@ -21,7 +21,7 @@
     </h1>
     <br>
     <p>
-      Our performance competition is named in honor of Margaret Bean Davis, a wonderful musician and composer who was also a long-time supporter of music in Marin County.   Each year talented young pianists compete before three distinguished local pianists in six levels:  Intermediate A, B, C and D and Advanced A and B.  The winners then perform at an annual Winners' Recital in the late spring.
+      Our performance competition is named in honor of Margaret Bean Davis, a wonderful musician and composer who was also a long-time supporter of music in Marin County.   Each year talented young pianists compete before three distinguished pianists in six levels:  Intermediate A, B, C and D and Advanced A and B.  The winners then perform at an annual Winners' Recital in the late spring.
     </p>
   </v-container>
 </template>
