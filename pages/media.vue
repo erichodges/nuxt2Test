6fe43476772782e9af412eef8a5cf70790dab431
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2018-10-16 16:25:10 -0700

    more photo grid

--- pages/media.vue
+++ pages/media.vue
@@ -2,19 +2,19 @@
   <v-container class="media-contianer">
     <div class="gallery">
 
-      <figure class="gallery__item gallery__item--1"><img src="/images/media/1.jpg" alt="gallery Image 1" class="gallery__img"></figure>
-      <figure class="gallery__item gallery__item--2"><img src="/images/media/2.jpg" alt="gallery Image 2" class="gallery__img"></figure>
-      <figure class="gallery__item gallery__item--3"><img src="/images/media/3.jpg" alt="gallery Image 3" class="gallery__img"></figure>
-      <figure class="gallery__item gallery__item--4"><img src="/images/media/4.jpg" alt="gallery Image 4" class="gallery__img"></figure>
-      <figure class="gallery__item gallery__item--5"><img src="/images/media/5.jpg" alt="gallery Image 5" class="gallery__img"></figure>
-      <figure class="gallery__item gallery__item--6"><img src="/images/media/6.jpg" alt="gallery Image 6" class="gallery__img"></figure>
-      <figure class="gallery__item gallery__item--7"><img src="/images/media/7.jpg" alt="gallery Image 7" class="gallery__img"></figure>
-      <figure class="gallery__item gallery__item--8"><img src="/images/media/8.jpg" alt="gallery Image 8" class="gallery__img"></figure>
-      <figure class="gallery__item gallery__item--9"><img src="/images/media/9.jpg" alt="gallery Image 9" class="gallery__img"></figure>
-      <figure class="gallery__item gallery__item--10"><img src="/images/media/10.jpg" alt="gallery Image 10" class="gallery__img"></figure>
-      <figure class="gallery__item gallery__item--11"><img src="/images/media/11.jpg" alt="gallery Image 11" class="gallery__img"></figure>
-      <figure class="gallery__item gallery__item--12"><img src="/images/media/12.jpg" alt="gallery Image 12" class="gallery__img"></figure>
-      <figure class="gallery__item gallery__item--13"><img src="/images/media/13.jpg" alt="gallery Image 13" class="gallery__img"></figure>  
+      <figure class="gallery__item gallery__item gallery__item--1"><img src="/images/media/1.jpg" alt="gallery Image 1"></figure>
+      <figure class="gallery__item gallery__item gallery__item--2"><img src="/images/media/2.jpg" alt="gallery Image 2"></figure>
+      <figure class="gallery__item gallery__item gallery__item--3"><img src="/images/media/3.jpg" alt="gallery Image 3"></figure>
+      <figure class="gallery__item gallery__item gallery__item--4"><img src="/images/media/4.jpg" alt="gallery Image 4"></figure>
+      <figure class="gallery__item gallery__item gallery__item--5"><img src="/images/media/5.jpg" alt="gallery Image 5"></figure>
+      <figure class="gallery__item gallery__item gallery__item--6"><img src="/images/media/6.jpg" alt="gallery Image 6"></figure>
+      <figure class="gallery__item gallery__item gallery__item--7"><img src="/images/media/7.jpg" alt="gallery Image 7"></figure>
+      <figure class="gallery__item gallery__item gallery__item--8"><img src="/images/media/8.jpg" alt="gallery Image 8"></figure>
+      <figure class="gallery__item gallery__item gallery__item--9"><img src="/images/media/9.jpg" alt="gallery Image 9"></figure>
+      <figure class="gallery__item gallery__item gallery__item--10"><img src="/images/media/10.jpg" alt="gallery Image 10"></figure>
+      <figure class="gallery__item gallery__item gallery__item--11"><img src="/images/media/11.jpg" alt="gallery Image 11"></figure>
+      <figure class="gallery__item gallery__item gallery__item--12"><img src="/images/media/12.jpg" alt="gallery Image 12"></figure>
+      <figure class="gallery__item gallery__item gallery__item--13"><img src="/images/media/13.jpg" alt="gallery Image 13"></figure>  
 
     </div>
     <br>
@@ -30,6 +30,7 @@ export default {
 }
 </script>
 
+
 <style lang="scss">
 
 .media-contianer {
@@ -39,18 +40,23 @@ export default {
 .gallery {
   
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(250, 1fr));
-  // grid-template-rows: repeat(250px 150px);
+  grid-template-columns: repeat(auto-fill, minmax(375px, 1fr));
+  grid-template-rows: repeat(8, vw);
   grid-gap: 1rem;
-  // padding: 1.5rem;
-
   
+
+  &__item {
+    &--1 {
+      grid-row: 1/1;
+      grid-column: 1/1;
+    }
+  }
 
   &__img {
     width: 100%;
     height: 100%;
     object-fit: cover;
-    // display: block;
+    display: block;
   }
 
 }
