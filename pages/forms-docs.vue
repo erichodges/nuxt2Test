a0d709f58cc3c08d5fc8cbe7690a8617c1e6f6d2
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2018-10-29 14:45:10 -0700

    working on new custom form

--- pages/forms-docs.vue
+++ pages/forms-docs.vue
@@ -1,26 +1,7 @@
 <template>
 <v-container>
-  <v-layout>    
-    <form name="contact" method="POST" netlify>
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+  <v-layout>
+  <h3>This form is under construction and should be ready relatively soon</h3>
   </v-layout>
   </v-container>
 </template>
