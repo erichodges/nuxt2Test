71985b5e11747d35062f3b6d4bd177550242da84
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2018-10-31 15:11:24 -0700

    fixed typo

--- pages/submitted.vue
+++ pages/submitted.vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-layout>
       <div>
-        <h3 class="reply">Thank you, Your Recital Registration form has been submitted.</h3>
+        <h3 class="reply">Thank you, your Recital Registration form has been submitted.</h3>
       </div>
     </v-layout>
   </v-container>
