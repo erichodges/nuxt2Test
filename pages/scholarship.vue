fb59aab63f91c4ba6c2d170b2f9f5dd8dc6066d5
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2018-12-04 12:53:25 -0800

    updated scholarship page

--- pages/scholarship.vue
+++ pages/scholarship.vue
@@ -1,34 +1,119 @@
 <template>
   <v-container class="scholar-header">
+    <div class="scholarDiv group">
     <h1>Discovery Scholarship</h1>
     <br>
+    <figure class="ds1">
+      <img width="230px" src="/images/scholarship/ds1.jpg" alt="">
+    </figure>
     <p>
-      In 1984 the Marin branch of the Music Teachers’ Association of California (MTAC) founded the Discovery Scholarship program, a California 501(c)3 non-profit corporation, for the purpose of providing private music lessons to children from low-income families. Discovery Scholarship attempts to fill that gap for at least some families. Discovery Scholarship started with three students in 1984 and has grown to twelve students.
+      In 1984 the Marin branch of the Music Teachers’ Association of California (MTAC) founded the Discovery scholarship program for the purpose of providing private music lessons to children from low-income families.  Please click the button above to download a copy of the current application form in English or Spanish.
     </p>
-      Lessons are offered in a wide variety of instruments. Recipients are chosen through a rigorous screening process involving an extensive written application, documentation of financial need, letters of recommendations from teachers (and others) and a personal interview with the three-member Scholarship Committee.
-    <p>
-      The Committee carefully reviews all aspects of the application and makes its decisions based on a wide variety of factors, with financial need being the uppermost. As long as the financial need remains and the student progresses satisfactorily, the Scholarship is guaranteed to continue through 12th grade.
-    </p>
-    <p>
-      Discovery students come from diverse backgrounds. Graduates of the program have become inventors, entrepreneurs, teachers, musicians, film composers, business professionals and photographers, among others. Students who remain in the program through senior year generally go on to college, including some of the best-known schools in the United States (Duke, George Washington University, Northwestern, Harvard, Columbia, the Culinary Institute of America at Hyde Park, etc.).
-    </p>
-    <p>
-      Discovery is financed through an annual fund-raising mailing, grants from several local non-profits and by many local Marin county residents who believe that music can offer a window into a world not generally available to low-income children.
-    </p>
+    <figure class="ds2">
+      <img height="230px" src="/images/scholarship/ds2.jpg" alt="">
+    </figure>
     <br>
+    <br>
+    <br>
+    <p>
+      Lessons are offered in piano, strings, winds and voice. As long as financial need guidelines are met and the student progresses satisfactorily, the Scholarship is guaranteed to continue through senior year in high school.
+    </p>
+    <figure class="ds3">
+      <img height="230px" src="/images/scholarship/ds3.jpg" alt="">
+    </figure>
+    <p>
+      Graduates of Discovery have attended colleges such as Harvard, Duke, George Washington, Columbia, Northwestern, Columbia, Wharton School of Business and Berkeley.  Post-college, our students have gone on to careers as inventors, business owners, entrepreneurs, classroom instructors, private teachers, professional musicians and film composers, among others.
+    </p>
+    <figure class="ds4">
+      <img height="230px" src="/images/scholarship/ds4.jpg" alt="">
+    </figure>
+    <p>
+      Discovery is financed by individual bequests, grants from local non-profits and by many local Marin county residents who believe that music can open doorways into a world not generally available to low-income students.
+    </p>
+    <!-- <figure class="ds5">
+      <img height="230px" src="/images/scholarship/ds5.jpg" alt="">
+    </figure> -->
+  </div>
+    <div class="appsDiv">
+      <br>
+
+      <h3>Application Download Links</h3>
+      <br>
+      <v-icon>cloud_download</v-icon>&nbsp;
+      <a class="link" href="https://drive.google.com/uc?export=download&id=1YxW-Sy4LAB_8ms9Zvf9CGup5Krljgz01">2018-19 Discovery Application</a>
+      <br>
+      <v-icon>cloud_download</v-icon>&nbsp;
+      <a class="link" href="https://drive.google.com/uc?export=download&id=1KWvce8apyOZ0vVdqHM-8PATJTigLt0_G">Aplicación de descubrimiento 2018-19</a>
+    </div>
   </v-container>
 </template>
 
 
 <script>
-export default {
-  
-}
+export default {};
 </script>
 
 <style lang="scss">
+.group:before,
+.group:after {
+  content: "";
+  display: table;
+}
+.group:after {
+  clear: both;
+}
+.group {
+  clear: both;
+  *zoom: 1;
+}
+
 .scholar-header {
   margin-top: 4rem;
   height: 100vh;
+}
+
+.scholarDiv {
+  overflow: hidden;
+}
+
+.ds1 {
+  float: right;
+  margin-left: 0.5rem;
+  margin-top: 0;
+  margin-bottom: 0;
+}
+.ds2 {
+  float: left;
+  margin: 0.5rem;
+  margin-left: 0;
+  margin-top: 0;
+}
+.ds3 {
+  float: right;
+  margin-right: 0;
+  margin-left: 0.5rem;
+}
+.ds4 {
+  float: left;
+  margin: 0.5rem;
+  margin-left: 0;
+}
+.ds5 {
+  float: left;
+  margin: 0.5rem;
+  margin-left: 2rem;
+}
+
+.appsDiv {
+  clear: both;
+}
+.dlLinks {
+  background-color: blue;
+}
+
+.link {
+  text-decoration: none !important;
+  color: white;
+  margin-left: 0;
 }
 </style>
